c1b98dd08b4b4b6465497fb27b5b265b531d0c5f
Meridian74 <meridian@freemail.hu>
2021-10-03 02:06:54 +0200

    Implement some new classes for redesigned app

---
+++ src/main/java/com/meridian/Adder.java
@@ -0,0 +1,18 @@
+package com.meridian;
+
+public class Adder extends CalculateBase {
+
+   public Adder() {
+   }
+
+   public Adder(double leftVal, double rightVal) {
+      super(leftVal, rightVal);
+   }
+
+   @Override
+   public void calculate() {
+      double value = getLeftVal() + getRightVal();
+      setResult(value);
+   }
+   
+}
---
+++ src/main/java/com/meridian/MathOperation.java
@@ -0,0 +1,8 @@
+package com.meridian;
+
+public enum MathOperation {
+   ADD,
+   SUBTRACT,
+   MULTIPLY,
+   DIVIDE
+}
